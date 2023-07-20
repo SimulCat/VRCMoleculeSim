cea6ec373a0a856b941e11102426bf32d7a454bf
John K N Murphy <j.murpheous@gmail.com>
2023-07-21 00:17:31 +1200

    Adding Trajectory Precalc/Lookup

--- UdonSharp/TrajectoryCalc.cs
+++ UdonSharp/TrajectoryCalc.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class TrajectoryCalc : UdonSharpBehaviour
 {
     [Header("Number of Lookup Points")]
     [SerializeField,Range(128,4096)]
     private int LookupPoints = 256;
+    Vector3[] launchVelocities;
+    bool isValid = false;
+
+    public float gratingDistance;
+    public Vector2 speedMaxMin;
+    public float gravity;
+    public bool hasGravity;
+
+    public void setupTrajectories(float speedMax, float speedMin, float gravity , bool hasGravity)
+    {
+        if ((launchVelocities == null) || (launchVelocities.Length != LookupPoints))
+            launchVelocities = new Vector3[LookupPoints];
+        float vDelta = speedMax - speedMin;
+        for (int i = 0; i < LookupPoints; i++)
+        {
+            float frac = (float)i / LookupPoints;
+            launchVelocities[i] = new Vector3(Mathf.Lerp(speedMin, speedMax, frac),0,0);
+        }
+        isValid = true;
+    }
     void Start()
     {
-        
+        setupTrajectories(speedMaxMin.x,speedMaxMin.y,gravity,hasGravity);
     }
 }
