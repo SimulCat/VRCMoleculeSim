2f0471c9514b0571fde7628f2b529796ae192ca3
John K N Murphy <j.murpheous@gmail.com>
2024-12-09 17:43:59 +1300

    Adding select particle

---
+++ UdonSharp/ParticleSelect.cs
@@ -0,0 +1,13 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class ParticleSelect : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
